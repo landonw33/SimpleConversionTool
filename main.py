7274d24313d2e9dbc73b905aba36e2ce48e8d644
Landon Walker <37057714+landonw33@users.noreply.github.com>
2022-03-08 22:22:03 -0700

    Add files via upload
    Added more user functionality. They can choose hours or days now.

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
-calculation_to_hours = 24
-name_of_units = "hours"
+# creating a function
+def days_to_units(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days is {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days is {num_of_days * 24 * 60} minutes"
+    else:
+        return f"unsupported unit"
 
 
-# function to do conversion
-def days_to_units(num_of_days):
-    return f"{num_of_days} days is {num_of_days * calculation_to_hours} {name_of_units}"
-
-# function to validate input
 def validate_and_execute():
     try:
-        user_input_number = int(num_of_days_element)
-        # only do conversion only for positive ints
+        user_input_number = int(days_and_unit_dictionary["days"])
         if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
             print(calculated_value)
-        # Won't do conversion for 0 days
         elif user_input_number == 0:
             print(f"You enter 0 days, please enter a positive number")
-        # only case left is negative
         else:
             print("you entered a negative, no conversion for you")
-    # When an invalid number is entered
     except ValueError:
         print("your input is not a valid number. Don't ruin my program")
 
 
-# variable must be define before while
 user_input = ""
 while user_input != "exit":
-    # get user input
-    user_input = input("Enter the number as a comma separated list:\n")
-    list_of_days = user_input.split(", ")
-    # (list_of_days)
-    # print(set(list_of_days))
-
-    # print(type(list_of_days))
-    # print(type(set(list_of_days)))
-    # for loop to accept multiple inputs in a list
-    # using set function to make sure list has no duplicates
-    for num_of_days_element in set(user_input.split(", ")):
-        validate_and_execute()
+    user_input = input("Enter the number of days and conversion unit:\n")
+    days_and_unit = user_input.split(":")
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    validate_and_execute()
 
 
+
+"""
+# example how access element of list
+my_list = ["20", "30"]
+print(my_list[1])
+# ... of dict
+my_dictionary = {"days": 20, "unit": "hours"}
+print(my_dictionary["unit"])"""
